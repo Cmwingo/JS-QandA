ee96ef11b8d4b42798ae98b140c2fd9992cd8bc8
Wingo <cmwingo@gmail.com>
2017-01-13 13:35:39 -0800

    Allow the user to add a new answer

--- app/routes/question.js
+++ app/routes/question.js
@@ -16,6 +16,11 @@ export default Ember.Route.extend({
       });
       question.save();
       this.transitionTo('index');
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
     }
   }
 });
